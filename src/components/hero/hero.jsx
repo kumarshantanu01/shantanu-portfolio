47b1bf9bf85fa35dc313a98dadbfca5b452619cd
kumarshantanu01 <shantanuk280@gmail.com>
2021-12-01 19:19:25 +0530

    fix: fav icon and name changed

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './hero.css';
-import { Button } from "@material-ui/core";
+import { Button, Link } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core";
 import { Facebook, GitHub, Instagram, LinkedIn, Twitter } from "@material-ui/icons";
 
@@ -12,6 +12,12 @@ const useStyles = makeStyles({
         fontWeight: "bold",
         '&:hover': {
             backgroundColor: '#091C34',
+            color: '#C6FF00',
+        },
+    },
+
+    socialmedia:{
+        '&:hover': {
             color: '#C6FF00',
         },
     }
@@ -30,12 +36,22 @@ const Hero = () =>{
                     <Button className={classes.button}>Download Resume</Button>
                 </div>
                 <div className="socialmediacontainer">
-                    <div className="socialmediaicons">
-                        <GitHub style={{ fontSize: 28 }}/>
-                        <LinkedIn style={{ fontSize: 30 }}/>
-                        <Twitter style={{ fontSize: 30 }}/>
-                        <Instagram style={{ fontSize: 30 }}/>
-                        <Facebook style={{ fontSize: 30 }}/>
+                    <div className="socialmediaicons" >
+                        <Link href='https://github.com/kumarshantanu01' target="_blank" color='inherit' className={classes.socialmedia}>
+                            <GitHub  style={{ fontSize: 28 }}/>
+                        </Link>
+                        <Link href='https://www.linkedin.com/in/kumar-shantanu01/' target="_blank" color='inherit' className={classes.socialmedia}>
+                            <LinkedIn style={{ fontSize: 30 }}/>
+                        </Link>
+                        <Link href='https://twitter.com/kumarshantanu01' target="_blank" color='inherit' className={classes.socialmedia}>
+                            <Twitter style={{ fontSize: 30 }}/>
+                        </Link>
+                        <Link href='https://www.instagram.com/kumar_shantanu_01/' target="_blank" color='inherit' className={classes.socialmedia}>
+                            <Instagram style={{ fontSize: 30 }}/>
+                        </Link>
+                        <Link href='https://www.facebook.com/shantanu.kumar.96930013' target="_blank" color='inherit' className={classes.socialmedia}>
+                            <Facebook style={{ fontSize: 30 }}/>
+                        </Link>
                     </div>
                 </div>
             </div>
