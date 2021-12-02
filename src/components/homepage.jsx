59cff415e43239d4df2d46fe8427fcf76e37772b
kumarshantanu01 <shantanuk280@gmail.com>
2021-12-02 15:30:24 +0530

    feat: scroll bar styled

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./hero/hero";
 import Navbar from "./navbar/navbar";
 import Aboutme from "./aboutMe/aboutMe";
+import './scrollbar.css'
 
 const Homepage = () =>{
     return(
