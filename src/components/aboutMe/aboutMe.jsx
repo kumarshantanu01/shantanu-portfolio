2c8312570fd3af3a724dbfdeb4947840a3a35323
kumarshantanu01 <shantanuk280@gmail.com>
2021-12-02 14:42:10 +0530

    feat: aboutme section styled and responsive

--- src/components/aboutMe/aboutMe.jsx
+++ src/components/aboutMe/aboutMe.jsx
@@ -6,7 +6,7 @@ const Aboutme = () =>{
         <>
             <div className="aboutme">
                 <div className="container">
-                    <div className="text" style={{ color: '#fff' }}>
+                    <div className="text" style={{ color: '#f2f2f2' }}>
                         <h2>Hola, I’m Kumar Shantanu!</h2>
                         <h1>I am a sophomore at NIT Rourkela pursuing Electrical Engineering.</h1>
                         <h3>I love web-development, programming and designing. Currently, I am exploring back-end development and learning DSA.</h3>
@@ -14,7 +14,7 @@ const Aboutme = () =>{
                     <div className="image">
                        <div className="card">
                         <img
-                                src="https://res.cloudinary.com/dzbdnlr0f/image/upload/v1638432819/shantanu-portfolio/image-aboutme_fgpvhb.png"
+                                src="https://res.cloudinary.com/dzbdnlr0f/image/upload/v1638436237/shantanu-portfolio/aboutme-image_cdhk0x.png"
                                 alt="about me"
                             />
                         </div>
