e42ff454f41f148bb5febc6732db265b2d9a7e33
kumarshantanu01 <shantanuk280@gmail.com>
2021-12-03 21:25:47 +0530

    feat: skills section design

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -1,35 +1,43 @@
-import { Grid } from "@material-ui/core";
+import {makeStyles, Typography } from "@material-ui/core";
 import React from "react";
-import './skills.css'
+import './skills.css';
 import Skillscard from "./skillsCard";
 import SkillsIcon from "./skillsIcon";
 
+const useStyles = makeStyles({
+    heading:{
+        fontSize: '60px',
+    }
+})
+
 const Skills = () =>{
+    const classes = useStyles();
     return(
         <>
             <div className='skillssection'>
                 <div className="heading">
-                    <h2>skills</h2>
+                    <Typography className={classes.heading}>Skills</Typography>
                 </div>
 
                 <div className='containerskills'>
-                    <div className='skillslogo'>
-                        <div className='skillsbox'>
-                        <Grid>
-                            {SkillsIcon.map(function ncards(val){
-                                return(
-                                    <Skillscard
-                                        logoname ={val.logoname}
-                                        logo ={val.logo}
-                                    />
-                                )
-                            })}
-                        </Grid>
-                        </div>  
-                    </div>
-                    <div className='imageskills'>
-                        <img src='https://res.cloudinary.com/dzbdnlr0f/image/upload/v1638525890/shantanu-portfolio/logo/image-removebg-preview_30_b4gnt1.png' alt='png'></img>
-                    </div>
+                        <div className='skillslogo'>
+                            <div className='skillsbox'>
+                                {SkillsIcon.map(function ncards(val){
+                                    return(
+                                        <Skillscard
+                                            logoname ={val.logoname}
+                                            logo ={val.logo}
+                                        />
+                                    )
+                                })}
+                            </div>  
+                        </div>
+                        
+                        <div className='imageskills'>
+                            <img src='https://res.cloudinary.com/dzbdnlr0f/image/upload/v1638525890/shantanu-portfolio/logo/image-removebg-preview_30_b4gnt1.png' alt='png'></img>
+                        </div>
+                
+                    
                 </div>
        
         </div>    
--- src/components/skills/skillsCard.jsx
+++ src/components/skills/skillsCard.jsx
@@ -1,16 +1,20 @@
+
 import React from "react";
+import './skills.css';
 
 const Skillscard = (props) =>{
     return(
         <>
-        <div className="card">
-            <div className="logoname">
+    
+            <div className="card">
+                <div className="icon">
+                    <img src={props.logo} alt='logo'/>
+                </div>
+                <div className="logoname">
                 <h2>{props.logoname}</h2>
+                </div>
             </div>
-            <div className="icon">
-                <img src={props.logo} alt='logo'/>
-            </div>
-        </div>
+        
         </>
     )
 }
