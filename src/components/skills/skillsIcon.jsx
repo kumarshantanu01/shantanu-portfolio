ebc9f242e1c0c124858934c8e60d176466a1ae90
kumarshantanu01 <shantanuk280@gmail.com>
2021-12-03 15:21:41 +0530

    feat: skills section data added

---
+++ src/components/skills/skillsIcon.jsx
@@ -0,0 +1,70 @@
+const SkillsIcon = [
+    {
+        id: '1',
+        logoname: 'HTML',
+        logo: 'https://res.cloudinary.com/dzbdnlr0f/image/upload/v1638523857/shantanu-portfolio/logo/HTML_hurmdw.png',
+    },
+
+    {
+        id: '2',
+        logoname: 'CSS',
+        logo: 'https://res.cloudinary.com/dzbdnlr0f/image/upload/v1638525025/shantanu-portfolio/logo/CSS_3_gcqprr.png',
+    },
+
+    {
+        id: '3',
+        logoname: 'JAVASCRIPT',
+        logo: 'https://res.cloudinary.com/dzbdnlr0f/image/upload/v1638523193/shantanu-portfolio/logo/javascript_ndbeku.svg',
+    },
+
+    {
+        id: '4',
+        logoname: 'REACTJS',
+        logo: 'https://res.cloudinary.com/dzbdnlr0f/image/upload/v1638523200/shantanu-portfolio/logo/react_hok3ay.png',
+    },
+
+    {
+        id: '5',
+        logoname: 'NODEJS',
+        logo: 'https://res.cloudinary.com/dzbdnlr0f/image/upload/v1638523194/shantanu-portfolio/logo/nodejs_wfm3fx.svg',
+    },
+
+    {
+        id: '6',
+        logoname: 'GITHUB',
+        logo: 'https://res.cloudinary.com/dzbdnlr0f/image/upload/v1638523193/shantanu-portfolio/logo/github_s5fqga.svg',
+    },
+
+    {
+        id: '7',
+        logoname: 'MONGODB',
+        logo: 'https://res.cloudinary.com/dzbdnlr0f/image/upload/v1638523194/shantanu-portfolio/logo/mongodb_1_lwic5t.svg',
+    },
+
+    {
+        id: '8',
+        logoname: 'GIT',
+        logo: 'https://res.cloudinary.com/dzbdnlr0f/image/upload/v1638523193/shantanu-portfolio/logo/git_l84wjh.svg',
+    },
+
+    {
+        id: '9',
+        logoname: 'FIGMA',
+        logo: 'https://res.cloudinary.com/dzbdnlr0f/image/upload/v1638523854/shantanu-portfolio/logo/FIGMA_aeccir.png',
+    },
+
+    {
+        id: '10',
+        logoname: 'PHOTOSHOP',
+        logo: 'https://res.cloudinary.com/dzbdnlr0f/image/upload/v1638523857/shantanu-portfolio/logo/PHOTOSHOP_bkqexv.png',
+    },
+
+    {
+        id: '11',
+        logoname: 'ADOBEXD',
+        logo: 'https://res.cloudinary.com/dzbdnlr0f/image/upload/v1638523854/shantanu-portfolio/logo/ADOBEXD_oimsfr.png',
+    },
+
+]
+
+export default SkillsIcon;
