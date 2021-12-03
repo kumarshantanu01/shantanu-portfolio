53648ad0fe280df37e5bddd541c72f8d3e652498
kumarshantanu01 <shantanuk280@gmail.com>
2021-12-03 15:41:37 +0530

    feat: skills section added

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -1,10 +1,38 @@
+import { Grid } from "@material-ui/core";
 import React from "react";
-import './skills.css';
+import './skills.css'
+import Skillscard from "./skillsCard";
+import SkillsIcon from "./skillsIcon";
 
 const Skills = () =>{
     return(
-    <>
-    Skills
+        <>
+            <div className='skillssection'>
+                <div className="heading">
+                    <h2>skills</h2>
+                </div>
+
+                <div className='containerskills'>
+                    <div className='skillslogo'>
+                        <div className='skillsbox'>
+                        <Grid>
+                            {SkillsIcon.map(function ncards(val){
+                                return(
+                                    <Skillscard
+                                        logoname ={val.logoname}
+                                        logo ={val.logo}
+                                    />
+                                )
+                            })}
+                        </Grid>
+                        </div>  
+                    </div>
+                    <div className='imageskills'>
+                        <img src='https://res.cloudinary.com/dzbdnlr0f/image/upload/v1638525890/shantanu-portfolio/logo/image-removebg-preview_30_b4gnt1.png' alt='png'></img>
+                    </div>
+                </div>
+       
+        </div>    
     </>
     )
 }
