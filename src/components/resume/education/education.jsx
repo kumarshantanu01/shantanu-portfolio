a857cc331308a3c6c8e0eac4c141de0a0a86eef0
kumarshantanu01 <shantanuk280@gmail.com>
2021-12-09 14:31:54 +0530

    feat: education section added

--- src/components/resume/education/education.jsx
+++ src/components/resume/education/education.jsx
@@ -1,9 +1,65 @@
 import React from "react";
+import { Branch, CardEducation, Degree, Educationsection, Educationsectionbottom, Educationsectiontop, Heading, Imagesection, InstituteName, Location, TextSection, Year } from "./educationElements";
 
 const Education = () => {
     return(
         <>
-          Education
+          <Educationsection>     
+
+            <Educationsectiontop>
+              <Heading>My Education</Heading>
+            </Educationsectiontop>
+
+            <Educationsectionbottom>
+                <CardEducation>
+
+                    <Imagesection>
+
+                    </Imagesection>
+
+                    <TextSection>
+                        <InstituteName>National Institute of Technology, Rourkela</InstituteName>
+                        <Degree>B.tech</Degree>
+                        <Branch>Electrical Engineering</Branch>
+                        <Location>Rourkela, India</Location>
+                        <Year>2020 - 2024</Year>
+                    </TextSection>
+
+                </CardEducation>
+
+                <CardEducation>
+
+                    <Imagesection>
+
+                    </Imagesection>
+
+                    <TextSection>
+                        <InstituteName>National Institute of Technology, Rourkela</InstituteName>
+                        <Degree>B.tech</Degree>
+                        <Branch>Electrical Engineering</Branch>
+                        <Location>Rourkela, India</Location>
+                        <Year>2020 - 2024</Year>
+                    </TextSection>
+
+                </CardEducation>
+
+                <CardEducation>
+
+                    <Imagesection>
+
+                    </Imagesection>
+
+                    <TextSection>
+                        <InstituteName>National Institute of Technology, Rourkela</InstituteName>
+                        <Degree>B.tech</Degree>
+                        <Branch>Electrical Engineering</Branch>
+                        <Location>Rourkela, India</Location>
+                        <Year>2020 - 2024</Year>
+                    </TextSection>
+
+                </CardEducation>
+            </Educationsectionbottom>
+          </Educationsection>
         </>
     )
 }
