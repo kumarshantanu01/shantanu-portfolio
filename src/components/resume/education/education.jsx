aeeebd5bf1a9ee284345a60cf0149af6fe8e7034
kumarshantanu01 <shantanuk280@gmail.com>
2021-12-09 16:22:34 +0530

    feat: education section styled

--- src/components/resume/education/education.jsx
+++ src/components/resume/education/education.jsx
@@ -14,14 +14,16 @@ const Education = () => {
                 <CardEducation>
 
                     <Imagesection>
-
+                        <div className="image">
+                        <img src="https://res.cloudinary.com/dzbdnlr0f/image/upload/v1639046271/shantanu-portfolio/resume/nit_rourkela_pfbwuw.png" alt="nitr"></img>
+                        </div>
                     </Imagesection>
 
                     <TextSection>
                         <InstituteName>National Institute of Technology, Rourkela</InstituteName>
                         <Degree>B.tech</Degree>
                         <Branch>Electrical Engineering</Branch>
-                        <Location>Rourkela, India</Location>
+                        <Location>Rourkela, Odisha, India</Location>
                         <Year>2020 - 2024</Year>
                     </TextSection>
 
@@ -30,15 +32,17 @@ const Education = () => {
                 <CardEducation>
 
                     <Imagesection>
-
+                        <div className="image">
+                        <img src="https://res.cloudinary.com/dzbdnlr0f/image/upload/v1639046271/shantanu-portfolio/resume/km_academy_xzengb.png" alt="nitr"></img>
+                        </div>
                     </Imagesection>
 
                     <TextSection>
-                        <InstituteName>National Institute of Technology, Rourkela</InstituteName>
-                        <Degree>B.tech</Degree>
-                        <Branch>Electrical Engineering</Branch>
-                        <Location>Rourkela, India</Location>
-                        <Year>2020 - 2024</Year>
+                        <InstituteName>K.M.Academy, Asarganj</InstituteName>
+                        <Degree>Higher Secondary</Degree>
+                        <Branch>Science</Branch>
+                        <Location>Munger, Bihar, India</Location>
+                        <Year>2018 - 2020</Year>
                     </TextSection>
 
                 </CardEducation>
@@ -46,15 +50,17 @@ const Education = () => {
                 <CardEducation>
 
                     <Imagesection>
-
+                        <div className="image">
+                        <img src="https://res.cloudinary.com/dzbdnlr0f/image/upload/v1639046271/shantanu-portfolio/resume/nda_munger_yojxfq.png" alt="nitr"></img>
+                        </div>
                     </Imagesection>
 
                     <TextSection>
-                        <InstituteName>National Institute of Technology, Rourkela</InstituteName>
-                        <Degree>B.tech</Degree>
-                        <Branch>Electrical Engineering</Branch>
-                        <Location>Rourkela, India</Location>
-                        <Year>2020 - 2024</Year>
+                        <InstituteName>Notre Dame Academy, Munger</InstituteName>
+                        <Degree>Matriculation</Degree>
+                        {/* <Branch>Electrical Engineering</Branch> */}
+                        <Location>Munger, Bihar, India</Location>
+                        <Year>2008 - 2018</Year>
                     </TextSection>
 
                 </CardEducation>
---
+++ src/components/resume/education/educationElements.js
@@ -0,0 +1,83 @@
+import styled from "styled-components";
+
+export const Educationsection  = styled.div`
+    background-color: #161625;
+`
+
+export const Educationsectiontop = styled.div`
+    padding: 2rem;
+`
+
+export const Educationsectionbottom = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+export const Heading = styled.div`
+    justify-content: center;
+    display: flex;
+    color: #C6FF00;
+    font-size: 45px;
+`
+
+export const CardEducation = styled.div`
+    width: 65%;
+    height: 30vh;
+    border-radius: 16px;
+    /* background: #004E64; */
+    background: linear-gradient(103.84deg, #004E64 3.07%, #007C78 98.55%);
+    display: flex;
+    flex-direction: row;
+    margin: 2rem 0 2rem 0;
+    overflow: hidden;
+`
+
+export const Imagesection = styled.div`
+    width: 40%;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    /* z-index: 0; */
+`
+
+export const TextSection = styled.div`
+    width: 60%;
+    color: #fff;
+    display: flex;
+    flex-direction: column;
+    /* align-items: center; */
+    justify-content: center;
+    padding: 2rem;
+`
+
+export const InstituteName = styled.div`
+    font-size: 25px;
+    font-weight: 500;
+`
+
+export const Degree = styled.div`
+    font-size: 20px;
+    font-weight: 400;
+    /* color: #c6ff00; */
+`
+
+export const Branch = styled.div`
+    font-size: 20px;
+    font-weight: 400;
+`
+
+export const Location = styled.div`
+    font-size: 18px;
+    font-weight:300;
+    color: #c6ff00;
+`
+
+export const Year = styled.div`
+    font-size: 18px;
+    font-weight: 300;
+    color: #c6ff00;
+`
+
+
