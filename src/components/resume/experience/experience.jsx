2d7a296737c1429799dac15fac20540258e3045e
kumarshantanu01 <shantanuk280@gmail.com>
2021-12-12 23:38:31 +0530

    feat: adding timeline section

--- src/components/resume/experience/experience.jsx
+++ src/components/resume/experience/experience.jsx
@@ -1,9 +1,53 @@
 import React from "react";
+import './experience.css';
 
 const Experience = () => {
     return(
         <>
-        
+        <section class="timeline-section">
+            <div class="timeline-items">
+                <div class="timeline-item">
+                <div class="timeline-dot"></div>
+                <div class="timeline-date">2020</div>
+                <div class="timeline-content">
+                    <h3>Title</h3>
+                    <p> WRITING ABOUT MY EXPERIENCE </p>
+                </div>
+            </div>
+            <div class="timeline-item">
+                <div class="timeline-dot"></div>
+                <div class="timeline-date">2020</div>
+                <div class="timeline-content">
+                    <h3>Title</h3>
+                    <p> WRITING ABOUT MY EXPERIENCE </p>
+                </div>
+            </div>
+            <div class="timeline-item">
+                <div class="timeline-dot"></div>
+                <div class="timeline-date">2020</div>
+                <div class="timeline-content">
+                    <h3>Title</h3>
+                    <p> WRITING ABOUT MY EXPERIENCE </p>
+                </div>
+            </div>
+            <div class="timeline-item">
+                <div class="timeline-dot"></div>
+                <div class="timeline-date">2020</div>
+                <div class="timeline-content">
+                    <h3>Title</h3>
+                    <p> WRITING ABOUT MY EXPERIENCE </p>
+                </div>
+            </div>
+            <div class="timeline-item">
+                <div class="timeline-dot"></div>
+                <div class="timeline-date">2020</div>
+                <div class="timeline-content">
+                    <h3>Title</h3>
+                    <p> WRITING ABOUT MY EXPERIENCE </p>
+                </div>
+            </div>
+            </div>
+        </section>
         </>
     )
 }
